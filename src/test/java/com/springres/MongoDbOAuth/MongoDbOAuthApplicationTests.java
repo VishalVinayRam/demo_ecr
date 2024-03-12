5b8b8fe912cda8b42e3cc10be38a8267b320cf12
linuxlite <vishalvinayram5432@gmail.com>
2024-03-12 15:03:10 +0530

    spring

---
+++ src/test/java/com/springres/MongoDbOAuth/MongoDbOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springres.MongoDbOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
